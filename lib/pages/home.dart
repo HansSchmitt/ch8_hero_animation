a2a8c3760e867d78c8c9138eef6e4e3d5be8262b
Rodolfo Schmitt <Rodo-Hans@Hotmail.com>
2019-11-20 22:55:20 -0600

    Initial commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'fly.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: GestureDetector(
+            child: Hero(
+              tag: 'format_paint',
+              child: Icon(
+                  Icons.format_paint,
+                color: Colors.lightGreen,
+                size: 120.0,
+              ),
+            ),
+            onTap: () {
+              Navigator.push(context,
+                MaterialPageRoute(builder: (context) => Fly()),
+              );
+            }
+          ),
+        ),
+      ),
+    );
+  }
+}
